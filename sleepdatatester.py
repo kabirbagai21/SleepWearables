e4a9e3b387a256328d971ad9e40f739e0ca2501d
kabirbagai21 <kbagai@ucsd.edu>
2022-03-21 20:05:31 -0500

    Update authorization and API Flow
    3/21

--- sleepdatatester.py
+++ sleepdatatester.py
@@ -1,37 +1,120 @@
 import requests
+import os
+
 
 
 TokenURL = "https://api.fitbit.com/oauth2/token"
+
+TokenFile = "/Users/kabirbagai/Desktop/Radicle-Sleep-Project/tokens.txt"
 
 clientID = "2385BF"
 clientSecret = "34dd55f271a7630dec75ba92f7b43413"
 encodedID_Secret = 'MjM4NUJGOjM0ZGQ1NWYyNzFhNzYzMGRlYzc1YmE5MmY3YjQzNDEz'
 
 
-AuthorizationCode = "51a3282d9dfc572bf44789096420197e556a5eaa"
+AuthorizationCode = "cf211490f386cf439984c160baddc8e700d23247"
+
+def WriteConfig(AccToken, RefToken):
+     
+
+  #Delete the old config file
+  os.remove(TokenFile)
+
+  #Open and write to the file
+  FileObj = open(TokenFile,'w')
+  FileObj.write(AccToken + "\n")
+  FileObj.write(RefToken + "\n")
+  FileObj.close()
 
 
-BodyText = {'code' : AuthorizationCode,
-            'redirect_uri' : 'http://localhost',
-            'client_id' : clientID,
-            'grant_type' : 'authorization_code'}
+def GetConfig():
+      
 
-headers = {'Authorization': 'Basic ' + encodedID_Secret, 
-    'Content-Type': 'application/x-www-form-urlencoded'}  
+  #Open the file
+  FileObj = open(TokenFile,'r')
+
+  #Read first two lines - first is the access token, second is the refresh token
+  AccToken = FileObj.readline()
+  RefToken = FileObj.readline()
+
+  #Close the file
+  FileObj.close()
+
+  #See if the strings have newline characters on the end.  If so, strip them
+  if (AccToken.find("\n") > 0):
+    AccToken = AccToken[:-1]
+  if (RefToken.find("\n") > 0):
+    RefToken = RefToken[:-1]
+
+  #Return values
+  return AccToken, RefToken
 
 
-req = requests.post(TokenURL, params=BodyText, headers= headers)
 
-postResponse = req.json() 
+#sends a post request to fitbit API with auth code in exchange for access tokens
+def getAccessToken(authCode):
 
-access_token = postResponse['access_token']
+    BodyText = {'code' : authCode,
+                'redirect_uri' : 'http://localhost',
+                'client_id' : clientID,
+                'grant_type' : 'authorization_code'}
 
-header = {'Authorization' : 'Bearer {}'.format(access_token)}
-response = requests.get("https://api.fitbit.com/1/user/-/sleep/date/2022-03-21.json", headers = header).json()
+    headers = {'Authorization': 'Basic ' + encodedID_Secret, 
+        'Content-Type': 'application/x-www-form-urlencoded'}  
 
 
+    req = requests.post(TokenURL, params=BodyText, headers= headers)
 
-for k, v in response.items():
-    print(k)
-    print(v)
-    print("\n")
+    postResponse = req.json() 
+
+    access_token = postResponse['access_token']
+    refresh_token = postResponse['refresh_token']
+
+    WriteConfig(access_token, refresh_token)
+
+
+def refreshAccessToken(refreshToken):
+    BodyText = {'grant_type' : 'refresh_token', 'refresh_token' : refreshToken}
+
+    headers = {'Authorization': 'Basic ' + encodedID_Secret, 
+        'Content-Type': 'application/x-www-form-urlencoded'} 
+
+    req = requests.post(TokenURL, params=BodyText, headers=headers)
+
+    postResponse = req.json() 
+
+    new_access_token = postResponse['access_token']
+    new_refresh_token = postResponse['refresh_token']
+
+    WriteConfig(new_access_token, new_refresh_token)
+
+
+def APICall(access_token, refresh_token):
+    header = {'Authorization' : 'Bearer {}'.format(access_token)}
+
+    try:
+        response = requests.get("https://api.fitbit.com/1/user/-/sleep/date/2022-03-21.json", headers = header)
+        response.raise_for_status()
+
+        response = response.json()
+        for k, v in response.items():
+            print(k)
+            print(v)
+            print("\n")
+
+
+    except requests.exceptions.HTTPError as e:
+       
+        if (e['errorType'] == 'expired token'):
+            refreshAccessToken(refresh_token)
+            new_result = GetConfig()
+            new_access_token = new_result[0]
+            new_refresh_token = new_result[1]
+            APICall(new_access_token, new_refresh_token)
+
+
+result = GetConfig()
+access_token = result[0]
+refresh_token = result[1]
+
+APICall(access_token, refresh_token)
